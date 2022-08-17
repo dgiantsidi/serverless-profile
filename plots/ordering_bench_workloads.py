35e194f656281fb6f0ca67a541ad3b2db2472f85
dgiantsidi <dimitra.giantsidi@gmail.com>
2022-08-17 09:44:23 +0000

    ...

---
+++ plots/ordering_bench_workloads.py
@@ -0,0 +1,32 @@
+from os import read
+import matplotlib.pyplot as plt
+import sys
+import csv
+plt.rcParams["figure.figsize"] = (25,12)
+#plt.rcParams["figure.figsize"] = (5,3)
+plt.rcParams.update({'font.size': 55})
+
+
+flexilog = [232, 210, 136]
+boki = [1105, 800, 600]
+
+
+workloads = ['10', '15', '50']
+
+plt.plot(flexilog, marker='o', linestyle='--', color='b', label='PMLog', markersize=20)
+plt.plot(boki, marker='*', linestyle='--', color='r', label='Boki', markersize=20)
+
+ax = plt.gca()
+#plt.yscale("log")
+plt.xticks(list(range(0, 7)))
+ax.set_xticklabels(workloads)
+
+plt.ylabel("Latency (usec)")
+plt.xlabel("Reads (%)")
+plt.tight_layout()
+#plt.legend(loc='best')
+#plt.legend(ncol = 1, loc='right', bbox_to_anchor=(0.5, 1.15), mode="expand")
+plt.legend(ncol = 2, loc='upper center', bbox_to_anchor=(0.5, 1.4))
+plt.savefig("ordering_bench_workloads.pdf", bbox_inches='tight', dpi = 200)
+
+#plt.show()
